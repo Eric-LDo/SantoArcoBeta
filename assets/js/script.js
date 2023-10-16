1f58413c6e4ec7d71695b5ccf32ea4b028ab38d6
Eric-LDo <121363351+Eric-LDo@users.noreply.github.com>
2023-10-16 14:54:03 -0300

    desenvolvimento do dia 16
    montei a tela de cadastro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
 
+//Mostrar Senha
+const senhaInput = document.querySelector(".senha");
+const conSenhaInput = document.querySelector(".conSenha");
+        const mostrarSenhaCheckbox = document.querySelector(".mostrarSenha");
+
+        mostrarSenhaCheckbox.addEventListener("change", function () {
+            senhaInput.type = this.checked ? "text" : "password";
+            conSenhaInput.type = this.checked ? "text" : "password";
+        });
+
+
+// Função para formatar o CPF
+function formatarCPF(cpf) {
+    cpf = cpf.replace(/\D/g, ''); // Remove caracteres não numéricos
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); // Insere o primeiro ponto
+    cpf = cpf.replace(/(\d{3})(\d)/, '$1.$2'); // Insere o segundo ponto
+    cpf = cpf.replace(/(\d{3})(\d{1,2})$/, '$1-$2'); // Insere o traço
+    return cpf;
+}
+
+// Adiciona um ouvinte de evento de entrada ao campo de CPF
+const cpfInput = document.getElementById("cpf");
+cpfInput.addEventListener("input", function () {
+    this.value = formatarCPF(this.value);
+});
